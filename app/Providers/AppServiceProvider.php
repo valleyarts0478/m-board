7d55bead3cbc6a608fae222fd659ef18e19fcc18
valleyarts0478 <arts047855@yahoo.co.jp>
2020-08-19 16:16:35 +0900

    content_tuika

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        \Schema::defaultStringLength(191);
+        \URL::forceScheme('http');
     }
 
     /**
