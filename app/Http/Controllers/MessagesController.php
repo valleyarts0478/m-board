b6f5458cd3df5eaf26bfbc741402d09e99ce9dac
valleyarts0478 <arts047855@yahoo.co.jp>
2020-08-20 17:25:10 +0900

    validation

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -46,7 +46,10 @@ class MessagesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'content' => 'required|max:191'
+        ]);
+
         $message = new Message;
         $message->content = $request->content;
         $message->save();
@@ -95,7 +98,11 @@ class MessagesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+        
         $message = Message::find($id);
         $message->content = $request->content;
         $message->save();
